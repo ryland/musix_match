0ebe5574265dde6906ceabc78763a844d5495457
ry <ry@misprint.org>
2014-11-26 16:42:13 -0500

    Expose more fields for Tracks.

--- lib/musix_match/models/track.rb
+++ lib/musix_match/models/track.rb
@@ -4,9 +4,11 @@ module MusixMatch
       include Model
       include Searcher
       
-      model_with_attributes :track_id, :track_mbid, :track_name, :track_mbid, :track_length, :lyrics_id, :instrumental, :subtitle_id,
-                            :artist_id, :artist_mbid, :artist_name, :artist_mbid,
-                            :album_name, :album_id, :album_coverart_100x100
+      model_with_attributes :track_id, :track_mbid, :track_name, :track_length, :lyrics_id, 
+                            :instrumental, :subtitle_id, :artist_id, :artist_mbid,
+                            :artist_name, :album_name, :album_id, :album_coverart_100x100, 
+                            :album_coverart_500x500, :has_lyrics, :explicit,
+                            :track_spotify_id
       
       def self.get_chart(options={})        
         API::TrackChart.get_chart(options)
